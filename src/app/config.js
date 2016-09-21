6ebeb31da24887e2d3ce9f4576de79a6eab29d83
Christian Schaf <cschaf@outlook.com>
2016-09-20 17:46:18 -0700

    Update API port to 1337

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('app.config', [])
-    .constant('API_URL', 'http://localhost:8080')
+    .constant('API_URL', 'http://localhost:1337')
 
     .constant('USER_ROLES', {
         all: '*',
