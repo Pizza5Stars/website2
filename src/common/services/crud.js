63e5c9f38b4b5c184d6980f2c1afe52da3aae82d
alexandre Morand <alexmorand26@gmail.com>
2016-09-28 10:11:25 -0700

    crud

--- src/common/services/crud.js
+++ src/common/services/crud.js
@@ -4,5 +4,8 @@ angular.module('services.crud', ['app.config'])
             createUser: function (customer) {
                 return $http.post(API_URL + '/customer', customer, {headers: {'Content-Type': 'application/json'}});
             }
+            //getIngredient: function (ingredient) {
+            //   return $http.get(API_URL + '/ingredient', ingredient, {headers: {'Content-Type': 'application/json'}});
+            //}
         }
     });
