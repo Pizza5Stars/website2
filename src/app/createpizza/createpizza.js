e4765284a5a2853032e41cf020540cf96dc04330
Christian Schaf <cschaf@outlook.com>
2016-10-10 14:38:03 -0700

    Add dependescies, helper function and a console.log to print all ingreadients from the server

--- src/app/createpizza/createpizza.js
+++ src/app/createpizza/createpizza.js
@@ -2,7 +2,6 @@ angular.module('app.createpizza', [
     'services.crud',
     'ui.router'
 ])
-
     .config(function config($stateProvider) {
         $stateProvider.state('createpizza', {
             url: '/createpizza',
@@ -13,13 +12,78 @@ angular.module('app.createpizza', [
 
                 }
             },
-            data: {pageTitle: 'Create your pizza'}
+            data: {pageTitle: 'Create your pizza'},
+            resolve: {
+                ingredients: function (CrudService) {
+                    return CrudService.getIngredients();
+                },
+                sizes: function (CrudService) {
+                    /*return CrudService.getSizes();*/
+                },
+                suggestions: function (CrudService) {
+                    /*return CrudService.getSuggestions();*/
+                }
+            }
         });
     })
 
-    .controller('CreatePizzaCtrl', function CreatePizza($scope) {
-        $scope.ingredients = CrudService.getIngredients();
+    .controller('CreatePizzaCtrl', function CreatePizza($scope, $state, ingredients, suggestions, sizes, CrudService) {
+        console.log(ingredients);
+        initScopeVariables();
+
+        function initScopeVariables() {
+            $scope.selectedIngredients = [];
+            // To get all types of cheese -> $scope.selectableCheese = $scope.getIngredientsByCategories(["Cheese"]);
+            $scope.ingredients = ingredients.data;
+            //$scope.suggestions = suggestions.data;
+            //$scope.sizes = sizes.data;
+            //$scope.selectedSize = sizes.data[0];
+            $scope.price = 0;
+        }
+
+        function getIngredientByName(name) {
+            for (var i = 0; i < $scope.ingredients.length; i++) {
+                if ($scope.ingredients[i].name === name) {
+                    return $scope.ingredients[i];
+                }
+            }
+        }
+
+        function createImagePathsFromIngredientNames() {
+            for (var i = 0; i < $scope.ingredients.length; i++) {
+                $scope.ingredients[i].imagePath = './assets/' + $scope.ingredients[i].name + '.png';
+            }
+        }
+
+        $scope.getIngredientsByCategories = function (categories) {
+            if (categories.constructor !== Array) {
+                categories = [categories]
+            }
+
+            var ingredients = [];
+            for (var i = 0; i < $scope.ingredients.length; i++) {
+                if (categories.indexOf($scope.ingredients[i].category) !== -1) {
+                    ingredients.push($scope.ingredients[i]);
+                }
+            }
+            return ingredients;
+        };
+
+        $scope.findIngredientByName = function (name) {
+            for (var i = 0; i < $scope.ingredients.length; i++) {
+                if ($scope.ingredients[i].name === name) {
+                    return $scope.ingredients[i];
+                }
+            }
+        };
+
+        $scope.resetIngredients = function () {
+            $scope.selectedIngredients.splice(0, $scope.selectedIngredients.length);
+        };
+
     });
+
+
 
 
 
